a97e58977f56f1772981a817edb6e8bd80ced447
hayoung5 <dalja6309@gmail.com>
2023-07-17 18:10:49 +0900

    debug controller

--- API-server/api/controllers/borrowController.js
+++ API-server/api/controllers/borrowController.js
@@ -11,22 +11,25 @@ const myWalletAddress = wallet.address;
 const { checkInput, checkAddress } = require('../utils/helper.js');
 
 const {
-  cEthContractAddress,
   cTokenContractAddress,
-  underlyingContractAddress,
-  comptrollerAddress,
-  priceFeedAddress,
-  assetName,
-  ethDecimals,
-  cEthDecimals,
 } = require('../utils/constants.js');
 
 const {cEth, cErc20, underlying, comptroller, priceFeed} = require('../utils/contracts.js');
 
+const ethDecimals = 18; // Ethereum has 18 decimal places
+let assetName, daiDecimals, cDaiDecimals, cEthDecimals;
+
+(async()=> {
+  assetName = await underlying.symbol(); // DAI
+  daiDecimals = await underlying.decimals();
+  cDaiDecimals = await cErc20.decimals();
+  cEthDecimals = await cEth.decimals();
+})();
+
 // 요청받은 cToken 마켓 진입
 const enterMarket = async (req, res) => {
     let { cTokenAddress } = req.body;
-    checkAddress(cTokenAddress);
+    checkAddress(res, cTokenAddress);
     let markets = [cTokenAddress];
     try {
         const txSigner = comptroller.connect(wallet);
@@ -43,10 +46,11 @@ const enterMarket = async (req, res) => {
 const getMyLiquidity = async (req, res) => {
     // const { walletAddress } = req.body;
     // checkAddress(walletAddress);
+    console.log("get liquidity");
     const walletAddress = myWalletAddress;
     try {
         let {1:liquidity} = await comptroller.getAccountLiquidity(walletAddress);
-        liquidity = Number(liquidity / ethDecimals);
+        liquidity = ethers.formatUnits(liquidity, ethDecimals);
         const message = `the account(${walletAddress}) has ${liquidity} of LIQUID assets (worth of USD) pooled in the protocol.`;
         res.status(200).json({ message: message });
         console.log(message);
@@ -61,10 +65,11 @@ const getMyCollateralFactor = async (req, res) => {
     const {cTokenAddress} = req.body;
     // const { walletAddress } = req.body;
     // checkAddress(walletAddress);
+    checkAddress(res, cTokenAddress);
     
     try {
         let {1:collateralFactor} = await comptroller.markets(cTokenAddress);
-        collateralFactor = Number(collateralFactor / ethDecimals)/100 ;
+        collateralFactor = ethers.formatUnits(collateralFactor, ethDecimals) * 100 ;
         const message = `can borrow up to ${collateralFactor}% of your TOTAL collateral`;
         res.status(200).json({ message: message });
         console.log(message);
@@ -79,8 +84,8 @@ const getPrice = async (req, res) => {
     const { assetSymbol } = req.body;
     try {
         let underlyingPriceInUsd = await priceFeed.price(assetSymbol);
-        underlyingPriceInUsd = underlyingPriceInUsd / 1e6; // Price feed provides price in USD with 6 decimal places
-        const message = `1 ${assetName} == ${underlyingPriceInUsd.toFixed(6)} USD`;
+        underlyingPriceInUsd = ethers.toNumber(underlyingPriceInUsd) / 1e6; // Price feed provides price in USD with 6 decimal places
+        const message = `1 ${assetName} == ${underlyingPriceInUsd} USD`;
         res.status(200).json({ message: message });
         console.log(message);
     } catch (error) {
@@ -94,8 +99,8 @@ const getPrice = async (req, res) => {
 const getEthBorrowRate = async (req, res) => {
     try {
         let borrowRate = await cEth.borrowRatePerBlock();
-        borrowRate = borrowRate / ethDecimals
-        const message = `\nYour borrowed amount INCREASES (${borrowRate} * borrowed amount) ETH per block.\nThis is based on the current borrow rate.`;
+        borrowRate = ethers.formatUnits(borrowRate, ethDecimals);
+        const message = `Your borrowed amount INCREASES (${borrowRate} * borrowed amount) ETH per block. \n This is based on the current borrow rate.`;
         res.status(200).json({ message: message });
         console.log(message);
     } catch (error) {
@@ -104,12 +109,12 @@ const getEthBorrowRate = async (req, res) => {
     }
 };
 
-// Eth의 borrow rate 반환
+// Dai의 borrow rate 반환
 const getErc20BorrowRate = async (req, res) => {
     try {
         let borrowRate = await cErc20.borrowRatePerBlock();
-        borrowRate = borrowRate / ethDecimals;
-        const message = `\nYour borrowed amount INCREASES (${borrowRate} * borrowed amount) ${assetName} per block.\nThis is based on the current borrow rate.`;
+        borrowRate =ethers.formatUnits(borrowRate, daiDecimals);
+        const message = `Your borrowed amount INCREASES (${borrowRate} * borrowed amount) ${assetName} per block. This is based on the current borrow rate.`;
         res.status(200).json({ message: message });
         console.log(message);
     } catch (error) {
@@ -124,8 +129,8 @@ const getEthBorrowBalance = async (req, res) => {
     // checkAddress(walletAddress);
     const walletAddress = myWalletAddress;
     try {
-        let balance = await cEth.borrowBalanceCurrent(walletAddress);
-        balance = balance / ethDecimals;
+        let balance = await cEth.borrowBalanceCurrent.staticCall(walletAddress);
+        balance = ethers.formatUnits(balance, ethDecimals);
         const message = `Borrow balance is ${balance} ETH`;
         res.status(200).json({ message: message });
         console.log(message);
@@ -141,8 +146,8 @@ const getErc20BorrowBalance = async (req, res) => {
     // checkAddress(walletAddress);
     const walletAddress = myWalletAddress;
     try {
-        let balance = await cErc20.borrowBalanceCurrent(walletAddress);
-        balance = balance / ethDecimals;
+        let balance = await cErc20.borrowBalanceCurrent.staticCall(walletAddress);
+        balance = ethers.formatUnits(balance, daiDecimals);
         const message = `Borrow balance is ${balance} ${assetName}`;
         res.status(200).json({ message: message });
         console.log(message);
@@ -156,13 +161,11 @@ const getErc20BorrowBalance = async (req, res) => {
 // 이더 대출 (borrowAmount는 이더 단위)
 const borrowEth = async (req, res) => {
   let { borrowAmount } = req.body;
-  // 입력값 확인
-  checkInput(borrowAmount);
-
+    
     try {
         console.log(`Trying to borrow ${borrowAmount} ETH`);
         const txSigner = cEth.connect(wallet);
-        let borrow = await txSigner.getFunction("borrow").send(BigInt(borrowAmount));
+        let borrow = await txSigner.getFunction("borrow").send(ethers.parseEther(String(borrowAmount)));
         const borrowResult = await borrow.wait(1);
         if (isNaN(borrowResult)) {
             const message = `${borrowAmount} ETH borrow successful.`
@@ -183,16 +186,17 @@ const borrowEth = async (req, res) => {
 // ERC20 대출 (borrowAmount는 토큰 단위)
 const borrowErc20 = async (req, res) => {
     let { borrowAmount } = req.body;
+    const unScaledAmount = borrowAmount;
     // 입력값 확인
-    checkInput(borrowAmount);
+    borrowAmount = checkInput(res, borrowAmount, daiDecimals);
   
     try {
         console.log(`Trying to borrow ${borrowAmount} ${assetName}`);
-        const txSigner = cToken.connect(wallet);
-        let borrow = await txSigner.getFunction("borrow").send(BigInt(borrowAmount)*ethDecimals);
+        const txSigner = cErc20.connect(wallet);
+        let borrow = await txSigner.getFunction("borrow").send(borrowAmount);
         const borrowResult = await borrow.wait(1);
         if (isNaN(borrowResult)) {
-            const message = `${borrowAmount} ${assetName} borrow successful.`
+            const message = `${unScaledAmount} ${assetName} borrow successful.`
             console.log(message);
             res.status(200).json({ message: message });
         } else {
@@ -210,22 +214,25 @@ const borrowErc20 = async (req, res) => {
 // 빌린 이더 상환 (repayAmount는 이더 단위)
 const repayEth = async (req, res) => {
     let { repayAmount } = req.body;
+    const unScaledAmount = repayAmount;
     // 입력값 확인
-    checkInput(repayAmount);
   
     try {
-        console.log(`Trying to repay ${repayAmount} ETH`);
+        console.log(`Trying to repay ${unScaledAmount} ETH`);
         const txSigner = cEth.connect(wallet);
-        let repay = await txSigner.getFunction("repayBorrow").send(BigInt(repayAmount)*ethDecimals);
-        const repayResult = await borrow.wait(1);
-        const failure = repayResult.events.find(_ => _.event === 'Failure');
-        if (!failure) {
-            const message = `${repayAmount} ETH repay successful.`
+        let repay = await txSigner.getFunction("repayBorrow").send({
+            value: ethers.toBeHex(ethers.parseEther(String(repayAmount)))
+        });
+        const repayResult = await repay.wait(1);
+        if (isNaN(repayResult)) {
+            const message = `${unScaledAmount} ETH repay successful.`
             console.log(message);
             res.status(200).json({ message: message });
         } else {
-                const errorCode = failure.args.error;
-                throw new Error(`repayBorrow error, code ${errorCode}`);
+            throw new Error(
+                `See https://compound.finance/docs/ctokens#ctoken-error-codes\n` +
+                `Code: ${repayResult}\n`
+            );
         }
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -233,29 +240,31 @@ const repayEth = async (req, res) => {
     }
 };
 
-// 빌린 이더 상환 (repayAmount는 이더 단위)
+// 빌린 DAI 상환 (repayAmount는 DAI 단위)
 const repayErc20 = async (req, res) => {
     let { repayAmount } = req.body;
+    const unScaledAmount = repayAmount;
     // 입력값 확인
-    checkInput(repayAmount);
+    repayAmount = checkInput(res, repayAmount, daiDecimals);
     try {
         // cTokenContract가 ERC20 토큰 가져갈 수 있게 승인부터 선행
         const txSigner = underlying.connect(wallet);
-        let approve = await txSigner.getFunction("approve").send(cTokenContractAddress, BigInt(repayAmount * ethDecimals));
+        let approve = await txSigner.getFunction("approve").send(cTokenContractAddress, repayAmount);
         await approve.wait(1);
         try {
-            console.log(`Trying to repay ${repayAmount} ETH`);
+            console.log(`Trying to repay ${unScaledAmount} ${assetName}`);
             const txSigner = cErc20.connect(wallet);
-            let repay = await txSigner.getFunction("repayBorrow").send(BigInt(repayAmount)*ethDecimals);
+            let repay = await txSigner.getFunction("repayBorrow").send(String(repayAmount));
             const repayResult = await repay.wait(1);
-            const failure = repayResult.events.find(_ => _.event === 'Failure');
-            if (!failure) {
-                const message = `${repayAmount} ${assetName} repay successful.`
+            if (isNaN(repayResult)) {
+                const message = `${unScaledAmount} ${assetName} repay successful.`
                 console.log(message);
                 res.status(200).json({ message: message });
             } else {
-                    const errorCode = failure.args.error;
-                    throw new Error(`repayBorrow error, code ${errorCode}`);
+                throw new Error(
+                    `See https://compound.finance/docs/ctokens#ctoken-error-codes\n` +
+                    `Code: ${repayResult}\n`
+                );
             }
         } catch (error) {
             res.status(500).json({ error: error.message });
--- API-server/api/controllers/supplyController.js
+++ API-server/api/controllers/supplyController.js
@@ -8,31 +8,36 @@ const provider = new ethers.JsonRpcProvider(process.env.JSON_PRC_URL);
 const privateKey = process.env.PRIVATE_KEY;
 const wallet = new ethers.Wallet(privateKey, provider);
 const myWalletAddress = wallet.address;
-const { checkInput } = require('../utils/helper.js');
+const { checkAddress, checkInput } = require('../utils/helper.js');
 
 const {
-  cEthContractAddress,
   cTokenContractAddress,
-  underlyingContractAddress,
-  comptrollerAddress,
-  priceFeedAddress,
-  assetName,
-  ethDecimals,
-  cEthDecimals,
 } = require('../utils/constants.js');
 
-const {cEth, cErc20, underlying, comptroller, priceFeed} = require('../utils/contracts.js');
+const {cEth, cErc20, underlying } = require('../utils/contracts.js');
+
+const ethDecimals = 18; // Ethereum has 18 decimal places
+let assetName, daiDecimals, cDaiDecimals, cEthDecimals;
+
+(async()=> {
+  assetName = await underlying.symbol(); // DAI
+  daiDecimals = await underlying.decimals();
+  cDaiDecimals = await cErc20.decimals();
+  cEthDecimals = await cEth.decimals();
+})();
 
 // 서버에 연결되어있는 지갑 계정의 ETH, cETH, DAI 자산을 로그
 const getMyBalance = async (req, res) => {
   try {
     console.log(`get My wallet's balances`);
-    let myWalletEthBalance = await provider.getBalance(myWalletAddress) / ethDecimals;
-    let myWalletCEthBalance = await cEth.balanceOf(myWalletAddress) / cEthDecimals;
-    let myWalletUnderlyingBalance = await underlying.balanceOf(myWalletAddress) / ethDecimals;
-    result = { ETH_Balance: ethers.toNumber(myWalletEthBalance), 
-              cETH_Balance: ethers.toNumber(myWalletCEthBalance), 
-              DAI_Balance: ethers.toNumber(myWalletUnderlyingBalance)
+    let myWalletEthBalance = await provider.getBalance(myWalletAddress);
+    let myWalletCEthBalance = await cEth.balanceOf(myWalletAddress);
+    let myWalletUnderlyingBalance = await underlying.balanceOf(myWalletAddress);
+    let myWalletCErc20Balance = await cErc20.balanceOf(myWalletAddress);
+    result = { ETH_Balance: ethers.formatUnits(myWalletEthBalance, ethDecimals),
+              cETH_Balance: ethers.formatUnits(myWalletCEthBalance, cEthDecimals),
+              DAI_Balance: ethers.formatUnits(myWalletUnderlyingBalance, daiDecimals),
+              cDAI_Balance: ethers.formatUnits(myWalletCErc20Balance, cDaiDecimals)
             };
     console.log(result);
     res.status(200).json({ message: 'get my balance successfully', result});
@@ -45,21 +50,18 @@ const getMyBalance = async (req, res) => {
 // req.body로 받은 지갑 주소의 ETH, cETH, DAI 자산을 로그
 const getBalance = async (req, res) => {
   const { walletAddress } = req.body;
-
-  try {
-    ethers.utils.getAddress(address);
-  } catch (error) {
-    return res.status(400).json({ error: '올바른 이더리움 주소값이 아닙니다' });
-  }
+  checkAddress(res, walletAddress);
 
   try {
     console.log(`get balances of wallet address : ${walletAddress}`);
-    let walletEthBalance = await provider.getBalance(walletAddress) / ethDecimals;
-    let walletCEthBalance = await cEth.balanceOf(walletAddress) / BigInt(1e8);
-    let walletUnderlyingBalance = await underlying.balanceOf(walletAddress) / ethDecimals;
-    result = { ETH_Balance: ethers.toNumber(walletEthBalance), 
-              cETH_Balance: ethers.toNumber(walletCEthBalance), 
-              DAI_Balance: ethers.toNumber(walletUnderlyingBalance)
+    let walletEthBalance = await provider.getBalance(walletAddress);
+    let walletCEthBalance = await cEth.balanceOf(walletAddress);
+    let walletUnderlyingBalance = await underlying.balanceOf(walletAddress);
+    let walletCErc20Balance = await cErc20.balanceOf(walletAddress);
+    result = { ETH_Balance: ethers.formatUnits(walletEthBalance, ethDecimals),
+              cETH_Balance: ethers.formatUnits(walletCEthBalance, cEthDecimals),
+              DAI_Balance: ethers.formatUnits(walletUnderlyingBalance, daiDecimals),
+              cDAI_Balance: ethers.formatUnits(walletCErc20Balance, cDaiDecimals)
             };
     console.log(result);
     res.status(200).json({ message: `get balance of ${walletAddress} successfully`, result });
@@ -73,28 +75,29 @@ const getBalance = async (req, res) => {
 // 이더 예치 (supplyAmount는 이더 단위)
 const supplyEth = async (req, res) => {
   let { supplyAmount } = req.body;
+  const unScaledAmount = supplyAmount;
   // 입력값 확인
-  checkInput(supplyAmount);
+  supplyAmount = checkInput(res, supplyAmount, ethDecimals);
 
   try {
     // 잔고 확인
-    let walletEthBalance = await provider.getBalance(myWalletAddress) / ethDecimals;
+    let walletEthBalance = await provider.getBalance(myWalletAddress);
     console.log(`wallet > supply amount ? ${walletEthBalance > supplyAmount}`);
     if (walletEthBalance <= supplyAmount) {
-      throw new Error(`insufficient ETH, my wallet balance : ${walletEthBalance}, supply amount you requested : ${supplyAmount}`);
+      throw new Error(`insufficient ETH, my wallet balance : ${walletEthBalance}, supply amount you requested : ${unScaledAmount}`);
     } else {
       // 예치
       try {
-        console.log("start to supply",supplyAmount);
+        console.log(`start to supply ${unScaledAmount} ETH`);
         const txSigner = cEth.connect(wallet);
         let mint = await txSigner.getFunction("mint").send({
           from : myWalletAddress,
           gasPrice: ethers.toBeHex(BigInt(20000000000)), // use ethgasstation.info (mainnet only)
-          value: ethers.toBeHex(BigInt(supplyAmount * ethDecimals))
+          value: ethers.toBeHex(supplyAmount)
         })
         await mint.wait(1);
-        console.log(`supply ${supplyAmount} ETH sucessfully`);
-        res.status(200).json({ message: `supply ${supplyAmount} ETH sucessfully`, mint });
+        console.log(`supply ${unScaledAmount} ETH successfully`);
+        res.status(200).json({ message: `supply ${unScaledAmount} ETH successfully`, mint });
       } catch (error) {
         console.error('Error executing supply ETH function:', error);
         res.status(500).json({ error: 'Failed to execute supply ETH function' });
@@ -109,21 +112,22 @@ const supplyEth = async (req, res) => {
 // ERC20 예치 (supplyAmount는 다이 단위)
 const supplyErc20 = async (req, res) => {
   let { supplyAmount } = req.body;
+  const unScaledAmount = supplyAmount;
   // 입력값 확인
-  checkInput(supplyAmount);
+  supplyAmount = checkInput(res, supplyAmount, daiDecimals);
 
   try {
     // 잔고 확인
     const txSigner = underlying.connect(wallet);
-    let approve = await txSigner.getFunction("approve").send(cTokenContractAddress, BigInt(supplyAmount * ethDecimals));
+    let approve = await txSigner.getFunction("approve").send(cTokenContractAddress, ethers.toBeHex(supplyAmount));
     await approve.wait(1);
     try {
-      console.log("start to supply",supplyAmount);
+      console.log(`start to supply ${unScaledAmount} ${assetName}`);
       const txSigner = cErc20.connect(wallet);
-      let mint = await txSigner.getFunction("mint").send(BigInt(supplyAmount * ethDecimals));
+      let mint = await txSigner.getFunction("mint").send(supplyAmount);
       await mint.wait(1);
-      console.log(`supply ${supplyAmount} ${assetName} sucessfully`);
-      res.status(200).json({ message: `supply ${supplyAmount} ${assetName} sucessfully`, mint });
+      console.log(`supply ${unScaledAmount} ${assetName} successfully`);
+      res.status(200).json({ message: `supply ${unScaledAmount} ${assetName} successfully`, mint });
     } catch (error) {
       console.error(`Error executing supply ${assetName} function:`, error);
       res.status(500).json({ error: `Error executing supply ${assetName} function` });
@@ -137,9 +141,10 @@ const supplyErc20 = async (req, res) => {
 // cToken <-> ERC20  교환 비율 반환
 const getCErc20ExchangeRate = async (req, res) => {
   try {
-    // 잔고 확인
-    let erCurrent = await cTokenContract.exchangeRateCurrent();
-    let exchangeRate = +erCurrent * ethDecimals * ethDecimals/ cEthDecimals;
+    const erCurrent = await cErc20.exchangeRateCurrent.staticCall();
+    // rate / 10^(18 + EthDecimals - cEthDecimals)
+    let exchangeRate = ethers.toNumber(erCurrent / BigInt(10**20));
+    exchangeRate /= Math.pow(10,8);
     let message = `Current exchange rate from c${assetName} to ${assetName}: ${exchangeRate}`;
     console.log(message);
     res.status(200).json({ message: message});
@@ -152,9 +157,10 @@ const getCErc20ExchangeRate = async (req, res) => {
 // cETH <-> ETH  교환 비율 반환
 const getCEthExchangeRate = async (req, res) => {
   try {
-    // 잔고 확인
-    let erCurrent = await cEth.exchangeRateCurrent();
-    let exchangeRate = +erCurrent * ethDecimals * ethDecimals/ cEthDecimals;
+    let erCurrent = await cEth.exchangeRateCurrent.staticCall();
+    // rate / 10^(18 + EthDecimals - cEthDecimals)
+    let exchangeRate = ethers.toNumber(erCurrent / BigInt(10**20));
+    exchangeRate /= Math.pow(10,8);
     let message = `Current exchange rate from cETH to Eth: ${exchangeRate}`;
     console.log(message);
     res.status(200).json({ message: message});
@@ -167,21 +173,22 @@ const getCEthExchangeRate = async (req, res) => {
 // cETH를 상환함으로써 예치했던 자산 반환 받음
 const redeemCEth = async (req, res) => {
   let { redeemAmount } = req.body;
+  const unScaledAmount = redeemAmount;
   // 입력값 확인
-  checkInput(supplyAmount);
+  redeemAmount = checkInput(res, redeemAmount, cEthDecimals);
 
   try {
     // cETH redeem
     const txSigner = cEth.connect(wallet);
     let redeem = await txSigner.getFunction("redeem").send(
-      ethers.toBeHex(BigInt(redeemAmount * cEthDecimals)),
+      ethers.toBeHex(redeemAmount),
       {
         from : myWalletAddress,
         gasPrice: ethers.toBeHex(BigInt(20000000000)),
       })
     await redeem.wait(1);
-    console.log(`redeem ${redeemAmount} cETH sucessfully`);
-    res.status(200).json({ message: `redeem ${redeemAmount} cETH sucessfully`, redeem });
+    console.log(`redeem ${unScaledAmount} cETH successfully`);
+    res.status(200).json({ message: `redeem ${unScaledAmount} cETH successfully`, redeem });
 
   } catch (error) {
     res.status(402).json({ error: error.message });
@@ -192,21 +199,22 @@ const redeemCEth = async (req, res) => {
 // 반환받을 Underlying 자산기준으로 cETH를 상환함으로써 예치했던 자산 반환
 const redeemCEthBasedUnderlying = async (req, res) => {
   let { redeemAmount } = req.body;
+  const unScaledAmount = redeemAmount;
   // 입력값 확인
-  checkInput(supplyAmount);
+  redeemAmount = checkInput(res, redeemAmount, ethDecimals);
 
   try {
     // cETH redeem
     const txSigner = cEth.connect(wallet);
     let redeem = await txSigner.getFunction("redeemUnderlying").send(
-      ethers.toBeHex(BigInt(redeemAmount * cEthDecimals)),
+      ethers.toBeHex(redeemAmount),
       {
         from : myWalletAddress,
         gasPrice: ethers.toBeHex(BigInt(20000000000)),
       })
     await redeem.wait(1);
-    console.log(`redeem cETH based on ${redeemAmount} ETH sucessfully`);
-    res.status(200).json({ message: `redeem cETH based on ${redeemAmount} ETH sucessfully`, redeem });
+    console.log(`redeem cETH based on ${unScaledAmount} ETH successfully`);
+    res.status(200).json({ message: `redeem cETH based on ${unScaledAmount} ETH successfully`, redeem });
 
   } catch (error) {
     res.status(402).json({ error: error.message });
@@ -217,21 +225,22 @@ const redeemCEthBasedUnderlying = async (req, res) => {
 // cErc20을 상환함으로써 예치했던 자산 반환 받음
 const redeemCErc20 = async (req, res) => {
   let { redeemAmount } = req.body;
+  const unScaledAmount = redeemAmount;
   // 입력값 확인
-  checkInput(supplyAmount);
+  redeemAmount = checkInput(res, redeemAmount, cDaiDecimals);
 
   try {
     // cETH redeem
-    const txSigner = cTokenContract.connect(wallet);
+    const txSigner = cErc20.connect(wallet);
     let redeem = await txSigner.getFunction("redeem").send(
-      ethers.toBeHex(BigInt(redeemAmount * cEthDecimals)),
+      ethers.toBeHex(redeemAmount),
       {
         from : myWalletAddress,
         gasPrice: ethers.toBeHex(BigInt(20000000000)),
       })
     await redeem.wait(1);
-    console.log(`redeem ${redeemAmount} c${assetName} sucessfully`);
-    res.status(200).json({ message: `redeem ${redeemAmount} c${assetName} sucessfully`, redeem });
+    console.log(`redeem ${unScaledAmount} c${assetName} successfully`);
+    res.status(200).json({ message: `redeem ${unScaledAmount} c${assetName} successfully`, redeem });
 
   } catch (error) {
     res.status(402).json({ error: error.message });
@@ -239,24 +248,25 @@ const redeemCErc20 = async (req, res) => {
   }
 };
 
-// cErc20을 상환함으로써 예치했던 자산 반환 받음
+// 환받을 Underlying 자산기준으로 cERC20를 상환함으로써 예치했던 자산 반환
 const redeemCErc20BasedUnderlying = async (req, res) => {
   let { redeemAmount } = req.body;
+  const unScaledAmount = redeemAmount;
   // 입력값 확인
-  checkInput(supplyAmount);
+  redeemAmount = checkInput(res, redeemAmount, daiDecimals);
 
   try {
     // cETH redeem
-    const txSigner = cTokenContract.connect(wallet);
+    const txSigner = cErc20.connect(wallet);
     let redeem = await txSigner.getFunction("redeemUnderlying").send(
-      ethers.toBeHex(BigInt(redeemAmount * cEthDecimals)),
+      ethers.toBeHex(redeemAmount),
       {
         from : myWalletAddress,
         gasPrice: ethers.toBeHex(BigInt(20000000000)),
       })
     await redeem.wait(1);
-    console.log(`redeem c${assetName} based on ${redeemAmount} ${assetName} sucessfully`);
-    res.status(200).json({ message: `redeem c${assetName} based on ${redeemAmount} ${assetName} sucessfully`, redeem });
+    console.log(`redeem c${assetName} based on ${unScaledAmount} ${assetName} successfully`);
+    res.status(200).json({ message: `redeem c${assetName} based on ${unScaledAmount} ${assetName} successfully`, redeem });
 
   } catch (error) {
     res.status(402).json({ error: error.message });
