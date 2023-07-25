fa353a759777922e0289427b03eb3bfcc4a8d37e
hayoung5 <dalja6309@gmail.com>
2023-07-25 16:47:48 +0900

    add morgan logger

--- API-server/server.js
+++ API-server/server.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const userRoutes = require('./api/routes/userRoutes');
 const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
 
+// 로그 파일 경로 및 스트림 설정
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
 // 미들웨어 등록
 app.use(express.json());
+
+// 로깅 설정
+app.use(morgan('combined', { stream: accessLogStream }));
 
 // 라우트 설정
 app.use('/', userRoutes);
